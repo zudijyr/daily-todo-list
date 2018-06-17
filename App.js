bdb2faa6335f4efe7387f456e15f3ac481d847bd
zydijyr <charliepye@gmail.com>
2018-06-17 13:11:21 -0700

    changes checkbox values to array
    much cleaner code this way

--- App.js
+++ App.js
@@ -5,35 +5,27 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = { 
-		itemOne: false, itemTwo: false, itemThree: false, itemFour: false, itemFive: false, itemSix: false, itemSeven: false,
-		itemSevenText: "Still some tasks remaining"
+		taskBools: [false, false, false, false, false, false],
+		isComplete: false,
+		isCompleteText: "Still some tasks remaining"
 	}
 	this.onCheckPress = this.onCheckPress.bind(this);
   }
+
   checkCallback() {
-	if (this.state.itemOne && this.state.itemTwo && this.state.itemThree
-	  && this.state.itemFour && this.state.itemFive && this.state.itemSix) {
-		this.setState({ itemSeven: true })
-		this.setState({ itemSevenText: "All Done! Congrats!" })
+	if ( this.state.taskBools.every(function(value) { return value == true }) ) {
+		this.setState({ isComplete: true });
+		this.setState({ isCompleteText: "All Done! Congrats!" });
 	} else {
-		this.setState({ itemSevenText: "Still some tasks remaining" })
+		this.setState({ isComplete: false });
+		this.setState({ isCompleteText: "Still some tasks remaining" })
 	}
   }
   
   onCheckPress(num, item) {
-	if (num == 1) {	
-		this.setState({ itemOne: !item }, () => this.checkCallback()) ;
-	} else if (num == 2) {	
-		this.setState({ itemTwo: !item }, () => this.checkCallback()) ;
-	} else if (num == 3) {	
-		this.setState({ itemThree: !item }, () => this.checkCallback()) ;
-	} else if (num == 4) {	
-		this.setState({ itemFour: !item }, () => this.checkCallback()) ;
-	} else if (num == 5) {	
-		this.setState({ itemFive: !item }, () => this.checkCallback()) ;
-	} else if (num == 6) {	
-		this.setState({ itemSix: !item }, () => this.checkCallback()) ;
-	}
+	let taskBools = [ ...this.state.taskBools ];
+		taskBools[num] = !item;
+		this.setState({ taskBools }, () => this.checkCallback());
   }
 
   render() {
@@ -43,9 +35,9 @@ export default class App extends Component {
           <Form>
             <Text>Daily ToDo List</Text>
             <ListItem>
-              <CheckBox checked={this.state.itemOne} color="red"
+              <CheckBox checked={this.state.taskBools[0]} color="red"
 			  	onPress={
-					() => this.onCheckPress(1, this.state.itemOne )
+					() => this.onCheckPress(0, this.state.taskBools[0] )
 				} />
               <Body>
                 <Input placeholder= "Aerobic Exercise"
@@ -54,9 +46,9 @@ export default class App extends Component {
               </Body>
             </ListItem>
             <ListItem>
-              <CheckBox checked={this.state.itemTwo} color="red"
+              <CheckBox checked={this.state.taskBools[1]} color="red"
 			  	onPress={
-					() => this.onCheckPress(2, this.state.itemTwo)
+					() => this.onCheckPress(1, this.state.taskBools[1])
 				} />
               <Body>
                 <Input placeholder="Anaerobic Exercise"
@@ -65,9 +57,9 @@ export default class App extends Component {
               </Body>
             </ListItem>
             <ListItem>
-              <CheckBox checked={this.state.itemThree} color="blue"
+              <CheckBox checked={this.state.taskBools[2]} color="blue"
 			  	onPress={
-					() => this.onCheckPress(3, this.state.itemThree)
+					() => this.onCheckPress(2, this.state.taskBools[2])
 				} />
               <Body>
                 <Input placeholder="Meditation"
@@ -76,9 +68,9 @@ export default class App extends Component {
               </Body>
             </ListItem>
             <ListItem>
-              <CheckBox checked={this.state.itemFour} color="blue"
+              <CheckBox checked={this.state.taskBools[3]} color="blue"
 			  	onPress={
-					() => this.onCheckPress(4, this.state.itemFour)
+					() => this.onCheckPress(3, this.state.taskBools[3])
 				} />
               <Body>
                 <Input placeholder="Practice Music"
@@ -87,9 +79,9 @@ export default class App extends Component {
               </Body>
             </ListItem>
             <ListItem>
-              <CheckBox checked={this.state.itemFive} color="green"
+              <CheckBox checked={this.state.taskBools[4]} color="green"
 			  	onPress={
-					() => this.onCheckPress(5, this.state.itemFive)
+					() => this.onCheckPress(4, this.state.taskBools[4])
 				} />
               <Body>
                 <Input placeholder="Study Foreign Language"
@@ -98,9 +90,9 @@ export default class App extends Component {
               </Body>
             </ListItem>
             <ListItem>
-              <CheckBox checked={this.state.itemSix} color="green"
+              <CheckBox checked={this.state.taskBools[5]} color="green"
 			  	onPress={
-					() => this.onCheckPress(6, this.state.itemSix)
+					() => this.onCheckPress(5, this.state.taskBools[5])
 				} />
               <Body>
                 <Input placeholder="Study Coding"
@@ -109,9 +101,9 @@ export default class App extends Component {
               </Body>
             </ListItem>
 			<ListItem>
-			  <CheckBox checked={this.state.itemSeven} color="black"/>
+			  <CheckBox checked={this.state.isComplete} color="black"/>
 			  <Body>
-			    <Text>{this.state.itemSevenText}</Text>
+			    <Text>{this.state.isCompleteText}</Text>
 			  </Body>
 			</ListItem>
           </Form>
