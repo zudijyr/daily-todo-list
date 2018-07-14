383ba3e568e9298cec9924129b7e892ff4a0ddb1
zydijyr <charliepye@gmail.com>
2018-07-14 13:59:49 -0700

    adds levels, progress, remaining counter to each task

--- App.js
+++ App.js
@@ -11,11 +11,12 @@ export default class DailyToDoList extends Component {
   constructor(props) {
     super(props)
     this.state = { 
-		taskBools: [false, false, false, false, false, false],
+		taskBools: [false, false, false, false, false, false, false],
 		isComplete: false,
 		isCompleteText: incompleteText,
-		taskLevels: [0,0,0,0,0,0,0],
-		taskProgress: [0,0,0,0,0,0,0],
+		taskLevels:    [0,0,0,5, 0,0,0],
+		taskProgress:  [0,0,2,34,0,0,2],
+		taskRemaining: [3,3,1,1, 3,3,1],
 	}
 	this.onCheckPress = this.onCheckPress.bind(this);
   }
@@ -43,32 +44,68 @@ export default class DailyToDoList extends Component {
 	//result should be a non-empty string-array of bools
 	if (!err && result && typeof(result) === 'string' && result !== '[]'){
 		let taskBools = JSON.parse(result);
-		this.setState({ taskBools }, () => this.checkCallback());
+		this.setState({ taskBools });
 	}
 	});
   }
 
   checkCallback() {
+	let taskProgress = [ ...this.state.taskProgress ];
+	let taskRemaining = [ ...this.state.taskRemaining ];
 	if ( this.state.taskBools.every(function(value) { return value === true }) ) {
 		this.setState({ isComplete: true });
 		this.setState({ isCompleteText: completeText });
+		taskProgress[6] += 1;
+		taskRemaining[6] -= 1;
 	} else {
 		this.setState({ isComplete: false });
 		this.setState({ isCompleteText: incompleteText });
+		taskProgress[6] -= 1;
+		taskRemaining[6] += 1;
 	}
+	this.setState({ taskProgress, taskRemaining }, () => this.levelsCallback(6));
 	this.retrieveItem('key').then((item) => {
 		//debug line
 	});
+  }
+
+  levelsCallback(num) {
+	let thisRemaining = this.state.taskRemaining[num];
+	let taskLevels = [ ...this.state.taskLevels ];
+	let taskProgress = [ ...this.state.taskProgress ];
+	let taskRemaining = [ ...this.state.taskRemaining ];
+	if (thisRemaining === 0) {
+		taskLevels[num] += 1;
+		taskProgress[num] = 0;
+		taskRemaining[num] = 7*taskLevels[num];
+		this.setState({ taskLevels, taskProgress, taskRemaining });
+	} else if (taskProgress[num] < 0) { //in case they cancel a level up
+		taskLevels[num] -= 1;
+		taskRemaining[num] = 1;
+		if (taskLevels[num] === 0) {
+			taskProgress[num] = 2;
+		} else {
+			taskProgress[num] = 7*taskLevels[num]-1;
+		}
+		this.setState({ taskLevels, taskProgress, taskRemaining });
+	}
   }
   
   onCheckPress(num, item) {
 	let taskBools = [ ...this.state.taskBools ];
 	let taskProgress = [ ...this.state.taskProgress ];
+	let taskRemaining = [ ...this.state.taskRemaining ];
 	taskBools[num] = !item;
-	taskProgress[num] += 1;
+	if (taskBools[num] === true) {
+		taskProgress[num] += 1;
+		taskRemaining[num] -= 1;
+	} else if (taskBools[num] === false) {
+		taskProgress[num] -= 1;
+		taskRemaining[num] += 1;
+	}
     this.storeItem('key', taskBools);
 	this.setState({ taskBools }, () => this.checkCallback());
-	this.setState({ taskProgress });
+	this.setState({ taskProgress, taskRemaining }, () => this.levelsCallback(num));
   }
 
   render() {
@@ -92,7 +129,9 @@ export default class DailyToDoList extends Component {
                 <Input placeholder= "Aerobic Exercise"
                   onChangeText={(text) => this.setState({ itemOneText: text })}
                   value={this.state.itemOneText} />
+			      <Text>Current level: {this.state.taskLevels[0]}</Text>
 			      <Text>Current progress: {this.state.taskProgress[0]}</Text>
+			      <Text>To next level: {this.state.taskRemaining[0]}</Text>
               </Body>
             </ListItem>
             <ListItem>
@@ -104,7 +143,9 @@ export default class DailyToDoList extends Component {
                 <Input placeholder="Anaerobic Exercise"
                   onChangeText={(text) => this.setState({ itemTwoText: text })}
                   value={this.state.itemTwoText} />
+			      <Text>Current level: {this.state.taskLevels[1]}</Text>
 			      <Text>Current progress: {this.state.taskProgress[1]}</Text>
+			      <Text>To next level: {this.state.taskRemaining[1]}</Text>
               </Body>
             </ListItem>
             <ListItem>
@@ -116,7 +157,9 @@ export default class DailyToDoList extends Component {
                 <Input placeholder="Meditation"
                   onChangeText={(text) => this.setState({ itemThreeText: text })}
                   value={this.state.itemThreeText} />
+			      <Text>Current level: {this.state.taskLevels[2]}</Text>
 			      <Text>Current progress: {this.state.taskProgress[2]}</Text>
+			      <Text>To next level: {this.state.taskRemaining[2]}</Text>
               </Body>
             </ListItem>
             <ListItem>
@@ -128,7 +171,9 @@ export default class DailyToDoList extends Component {
                 <Input placeholder="Practice Music"
                   onChangeText={(text) => this.setState({ itemFourText: text })}
                   value={this.state.itemFourText} />
+			      <Text>Current level: {this.state.taskLevels[3]}</Text>
 			      <Text>Current progress: {this.state.taskProgress[3]}</Text>
+			      <Text>To next level: {this.state.taskRemaining[3]}</Text>
               </Body>
             </ListItem>
             <ListItem>
@@ -140,7 +185,9 @@ export default class DailyToDoList extends Component {
                 <Input placeholder="Study Foreign Language"
                   onChangeText={(text) => this.setState({ itemFiveText: text })}
                   value={this.state.itemFiveText} />
+			      <Text>Current level: {this.state.taskLevels[4]}</Text>
 			      <Text>Current progress: {this.state.taskProgress[4]}</Text>
+			      <Text>To next level: {this.state.taskRemaining[4]}</Text>
               </Body>
             </ListItem>
             <ListItem>
@@ -152,14 +199,18 @@ export default class DailyToDoList extends Component {
                 <Input placeholder="Study Coding"
                   onChangeText={(text) => this.setState({ itemSixText: text })}
                   value={this.state.itemSixText} />
+			      <Text>Current level: {this.state.taskLevels[5]}</Text>
 			      <Text>Current progress: {this.state.taskProgress[5]}</Text>
+			      <Text>To next level: {this.state.taskRemaining[5]}</Text>
               </Body>
             </ListItem>
 			<ListItem>
 			  <CheckBox checked={this.state.isComplete} color="black"/>
 			  <Body>
 			    <Text>Finished all daily tasks</Text>
+			    <Text>Current level: {this.state.taskLevels[6]}</Text>
 			    <Text>Current progress: {this.state.taskProgress[6]}</Text>
+			    <Text>To next level: {this.state.taskRemaining[6]}</Text>
 			  </Body>
 			</ListItem>
           </Form>
